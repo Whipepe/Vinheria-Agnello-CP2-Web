db03130fa2aa17c53393fe684e91cf42674f2e0f
Ricardo Salmerón <ricardosalmeronps@gmail.com>
2026-03-31 15:10:31 -0300

    [Feat]: Adicionando um sistema de cadastro de vinhos utilizando javascript

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+nome = prompt("Qual é o nome do vinho? ")
+tipo = prompt("Qual é o tipo do vinho? ")
+safra = prompt("Qual é a safra do vinho? ")
+quantidade = prompt("Qual é a quantidade de vinhos? ")
+console.log([nome, tipo, safra, quantidade])
