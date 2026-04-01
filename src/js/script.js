3d6e6b6f880368b50028b45b91b6aa15d4d1b96b
Gustavo Ferreira Tavares <ferreiratavaresg@gmail.com>
2026-04-01 17:14:30 -0300

    [feat]Adição do Link GitHub Pages no README

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-nome = prompt("Qual é o nome do vinho? ");
-tipo = prompt("Qual é o tipo do vinho? ");
-safra = prompt("Qual é a safra do vinho? ");
-quantidade = prompt("Qual é a quantidade de vinhos? ");
-console.log([nome, tipo, safra, quantidade]);
-alert("Para acessar as informações do seu cadastro, por gentileza olhe o console");
